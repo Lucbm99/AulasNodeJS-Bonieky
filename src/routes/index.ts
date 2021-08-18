b41a7e664c50e24a89ed2dae8622f37143574433
Lucbm99 <lbaradelmarchiori@gmail.com>
2021-08-17 22:36:17 -0300

    Início de prática com mustache - condicionais e laços de repetição

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,15 +3,70 @@ import {Router, Request, Response} from 'express';
 const router = Router();
 
 router.get('/', (request: Request, response: Response) => {
-    response.render('home');
+    let idade: number = 66;
+    let showOld: boolean = false;
+    
+
+    if(idade > 50) {
+        showOld = true;
+    }
+
+
+    response.render('home', {
+        name: 'Lucas',
+        idade: 22,
+        showOld, 
+        products: [
+            {title: 'Arroz', price: 10},
+            {title: 'Feijao', price: 8},
+            {title: 'Manga', price: 5}
+        ], 
+        frasesDoDia: [
+            'Alguma coisa muito legal',
+            'Outra frase qualquer'
+        ]
+    });
 });
 
 router.get('/contato', (request: Request, response: Response) => {
-    response.send("Formulário de contato");
+    let idade: number = 20;
+    let podeBeber: boolean = false;
+
+    if(idade >= 18) {
+        podeBeber = true;
+    }
+    response.render('contato', {
+        name: 'Lucas', 
+        lastName: 'Baradel Marchiori',
+        podeBeber,
+        linguagens: [
+            'C#', 'C++', 'Python', 'Scala', 'Ruby', 'PHP', 'Java'
+        ]
+    });
 });
 
 router.get('/sobre', (request: Request, response: Response) => {
-    response.send("Página institucional sobre a empresa");
+
+    let tipoHamburguer: string = 'Vegetariano';
+    let isVegan: boolean = false;
+
+    if(tipoHamburguer === 'Vegetariano') {
+        isVegan = true;
+    }
+
+    let about = {
+        texto1: 'Eu estou visualizando a página sobre',
+        texto2: 'E agora eu quero sair daqui!',
+        isVegan,
+        materiaisEscritorio: [
+            {nome: 'Caneta', preco: 1.33},
+            {nome: 'Apontador', preco: 2.45},
+            {nome: 'Grampeador', preco: 28},
+            {nome: 'Marca texto', preco: 3.43}
+        ]
+    }
+    
+    response.render('sobre', about);
 });
 
 router.get('/empresa', (request: Request, response: Response) => {
